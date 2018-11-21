1852850c31693572af7f70770f80307f3c1271c9
Nuno Gervasio <nunogee@gmail.com>
2018-11-21 19:05:33 +0100

    data is displayed using a table in the card

--- src/Card.js
+++ src/Card.js
@@ -4,12 +4,35 @@ const Card = props => {
   return (
     // <div className="card">
     <div className="margins">
-      <p>Flight Number: {props.number}</p>
-      <p>Flight Name: {props.name}</p>
-      <p>Flight Year: {props.year}</p>
+      <table>
+        <col width="30%" />
+        <col width="70%" />
+        <tr>
+          <td>Flight Number: </td>
+          <td>{props.number}</td>
+        </tr>
+        <tr>
+          <td>Flight Name: </td>
+          <td>{props.name}</td>
+        </tr>
+        <tr>
+          <td>Flight Year: </td>
+          <td>{props.year}</td>
+        </tr>
+      </table>
     </div>
     // </div>
   );
 };
 
 export default Card;
+
+{
+  /* <div className="margins">
+    <table>
+      <p>Flight Number: {props.number}</p>
+      <p>Flight Name: {props.name}</p>
+      <p>Flight Year: {props.year}</p>
+      </table>
+    </div> */
+}
